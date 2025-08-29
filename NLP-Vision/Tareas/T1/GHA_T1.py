db0904a30690a5045016d5ee30318737e17ad60c
Gustavo-HA <gustavoha40@gmail.com>
2025-08-29 00:19:41 -0600

    tarea 1 avance 7 puntos

---
+++ NLP-Vision/Tareas/T1/GHA_T1.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus.stopwords import 
+
+class Tarea1:
+    def __init__(self, data_filename):
+        self.data = pd.read_csv(data_filename)
+
+    def no1(self):
+        """Descripcion del Corpus"""
+        
+        # Punto 1
+        documentos = self.data["Review"].tolist()
+        n = len(documentos)
+        tokenizer = RegexpTokenizer(r'\w+')
+        doc_tokens = [tokenizer.tokenize(doc) for doc in documentos]
+        tokens = [token for doc in doc_tokens for token in doc]
+        vocab = set(tokens)
+        print(f"Numero de documentos: {n}")
+        print(f"Numero de tokens en el corpus: {len(tokens)}")
+        print(f"Numero de palabras en el vocabulario: {len(vocab)}")
+
+        # Punto 2
+        # Hapax legomena y su proporcion.
+        hapax_legomena = [word for word in vocab if tokens.count(word) == 1]
+        print(f"Numero de hapax legomena: {len(hapax_legomena)}")
+        print(f"Proporcion de hapax legomena con el vocabulario: {len(hapax_legomena) / len(vocab) * 100 if vocab else 0:.5f}")
+        
+        # Punto 3
+        # Porcentaje de stopwords

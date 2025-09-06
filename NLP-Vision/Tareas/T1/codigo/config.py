792bc30987468784a9ecdcc71331524ccd940199
Gustavo-HA <gustavoha40@gmail.com>
2025-09-06 15:24:03 -0600

    Refactor data file path definition for consistency

--- NLP-Vision/Tareas/T1/codigo/config.py
+++ NLP-Vision/Tareas/T1/codigo/config.py
@@ -1,5 +1,4 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(BASE_DIR, '..', 'data')
-DATA_FILE = os.path.join(DATA_DIR, 'meia_data.csv')
+DATA_FILE = os.path.join(BASE_DIR, '../data', 'meia_data.csv')
